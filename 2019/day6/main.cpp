92dbd24fdaa5178402d86aca136b343d702c2732
Jarosław Bielski <bielski.j@gmail.com>
2019-12-14 20:56:37 +0100

    Added graph implementation

--- 2019/day6/main.cpp
+++ 2019/day6/main.cpp
@@ -6,93 +6,30 @@
 
 #include "utils/utils.h"
 #include "utils/file.h"
+#include "utils/graph.h"
 
 #define DEBUG_LEVEL 4
 #include "common/debug.h"
 
 
-struct Node {
-	std::vector<Node *> neighbours;
+class Object : public Graph::Vertex {
+	public:
+		Object(const std::string &name) : Graph::Vertex(), name(name) {
+		}
 
-	Node() {
-	}
+		std::string getLabel() {
+			return name;
+		}
+
+	private:
+		std::string name;
 };
 
 
-static void _dijkstra(std::vector<Node *> &nodes, Node *root, std::map<Node *, int> &dist, std::map<Node *, Node *> &prev) {
-	std::vector<Node *> q;
-
-	dist.clear();
-	prev.clear();
-
-	for (auto v = nodes.begin(); v != nodes.end(); v++) {
-		dist[*v] = INT_MAX;
-		prev[*v] = NULL;
-
-		q.push_back(*v);
-	}
-
-	dist[root] = 0;
-
-	while (! q.empty()) {
-		Node *u;
-
-		{
-			std::vector<Node *>::iterator uIt = q.begin();
-
-			for (auto it = q.begin(); it != q.end(); it++) {
-				if (dist[*it] < dist[*uIt]) {
-					uIt = it;
-				}
-			}
-
-			u = *uIt;
-
-			q.erase(uIt);
-		}
-
-		for (auto v = u->neighbours.begin(); v != u->neighbours.end(); v++) {
-			int alt = dist[u] + 1;
-
-			if (alt < dist[*v]) {
-				dist[*v] = alt;
-				prev[*v] = u;
-			}
-		}
-	}
-}
-
-
-static void _bfs(Node *root, std::map<Node *, int> &dist) {
-	std::map<Node *, bool> visited;
-
-	std::queue<Node *> q;
-
-	dist[root] = 0;
-	visited[root] = true;
-
-	q.push(root);
-
-	while (! q.empty()) {
-		Node *x = q.front();
-
-		q.pop();
-
-		for (auto child = x->neighbours.begin(); child != x->neighbours.end(); child++) {
-			if (visited[*child]) {
-				continue;
-			}
-
-			dist[*child] = dist[x] + 1;
-			q.push(*child);
-			visited[*child] = true;
-		}
-	}
-}
-
-
 int main(int argc, char *argv[]) {
-	std::map<std::string, Node *> objectsMap;
+	std::map<std::string, Object *> objectsMap;
+
+	std::vector<Graph::Edge> edges;
 
 	{
 		std::vector<std::string> map = File::readAllLines(argv[1]);
@@ -101,31 +38,30 @@ int main(int argc, char *argv[]) {
 			std::vector<std::string> entry = utils::strTok(*it, ')');
 
 			if (objectsMap.find(entry[0]) == objectsMap.end()) {
-				objectsMap[entry[0]] = new Node();
+				objectsMap[entry[0]] = new Object(entry[0]);
 			}
 
 			if (objectsMap.find(entry[1]) == objectsMap.end()) {
-				objectsMap[entry[1]] = new Node();
+				objectsMap[entry[1]] = new Object(entry[1]);
 			}
 		}
 
 		for (auto it = map.begin(); it != map.end(); it++) {
 			std::vector<std::string> entry = utils::strTok(*it, ')');
 
-			Node *l = objectsMap.find(entry[0])->second;
-			Node *r  = objectsMap.find(entry[1])->second;
+			Object *l = objectsMap.find(entry[0])->second;
+			Object *r = objectsMap.find(entry[1])->second;
 
-			l->neighbours.push_back(r);
-			r->neighbours.push_back(l);
+			edges.push_back(Graph::Edge(l, r, 1, true));
 		}
 	}
 
 	{
-		Node *root = objectsMap["COM"];
+		Graph g(edges);
 
-		std::map<Node *, int> dist;
+		Object *root = objectsMap["COM"];
 
-		_bfs(root, dist);
+		std::map<Graph::Vertex *, float> dist = g.bfs(root);
 
 		int sum = 0;
 		for (auto it = dist.begin(); it != dist.end(); it++) {
@@ -136,17 +72,14 @@ int main(int argc, char *argv[]) {
 	}
 
 	{
-		std::vector<Node *> nodes;
 
-		std::map<Node *, int>    dist;
-		std::map<Node *, Node *> prev;
+		std::map<Graph::Vertex *, float>           dist;
+		std::map<Graph::Vertex *, Graph::Vertex *> prev;
 
-		for (auto it = objectsMap.begin(); it != objectsMap.end(); it++) {
-			nodes.push_back(it->second);
-		}
+		Graph g(edges);
 
-		_dijkstra(nodes, objectsMap["YOU"], dist, prev);
+		g.dijkstra(objectsMap["YOU"], dist, prev);
 
-		PRINTF(("PART B: %d", dist[objectsMap["SAN"]] - 2));
+		PRINTF(("PART B: %d", (int)dist[objectsMap["SAN"]] - 2));
 	}
 }
