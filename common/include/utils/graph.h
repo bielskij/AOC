6ca1921c8cf8cb568599a2e31b7fb97cc729f66b
Jarosław Bielski <bielski.j@gmail.com>
2019-12-14 20:56:37 +0100

    Added graph implementation

---
+++ common/include/utils/graph.h
@@ -0,0 +1,142 @@
+#ifndef UTILS_GRAPH_H_
+#define UTILS_GRAPH_H_
+
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+
+
+class Graph {
+	public:
+		class Vertex {
+			public:
+				Vertex() {
+				}
+
+				virtual ~Vertex() {
+				}
+
+				virtual std::string getLabel() = 0;
+		};
+
+		class Edge {
+			public:
+				Edge(Vertex *src, Vertex *dst, float weight, bool bothDirections) {
+					this->src            = src;
+					this->dst            = dst;
+					this->weight         = weight;
+					this->bothDirections = bothDirections;
+				}
+
+				Vertex *getSrc() const {
+					return this->src;
+				}
+
+				Vertex *getDst() const {
+					return this->dst;
+				}
+
+				float getWeight() const {
+					return this->weight;
+				}
+
+				bool isBothDirection() const {
+					return this->bothDirections;
+				}
+
+			private:
+				Vertex *src;
+				Vertex *dst;
+				float   weight;
+				bool    bothDirections;
+		};
+
+		Graph(const std::vector<Edge> &edges) {
+			for (auto edge = edges.begin(); edge != edges.end(); edge++) {
+				// TODO: Check for duplicates
+				this->adjVertices[edge->getSrc()].push_back(std::pair<Vertex *, float>(edge->getDst(), edge->getWeight()));
+
+				if (edge->isBothDirection()) {
+					this->adjVertices[edge->getDst()].push_back(std::pair<Vertex *, float>(edge->getSrc(), edge->getWeight()));
+				}
+			}
+		}
+
+		std::map<Vertex *, float> bfs(Vertex *root) {
+			std::map<Vertex *, float> ret;
+
+			{
+				std::map<Vertex *, bool> visited;
+				std::queue<Vertex *> queue;
+
+				ret[root]     = 0;
+				visited[root] = true;
+
+				queue.push(root);
+
+				while (! queue.empty()) {
+					Vertex *vertex = queue.front(); queue.pop();
+
+					for (auto v = adjVertices[vertex].begin(); v != adjVertices[vertex].end(); v++) {
+						if (! visited[v->first]) {
+							ret[v->first] = ret[vertex] + v->second;
+							queue.push(v->first);
+							visited[v->first] = true;
+						}
+					}
+				}
+			}
+
+			return ret;
+		}
+
+		void dijkstra(Vertex *root, std::map<Vertex *, float> &dist, std::map<Vertex *, Vertex *> &prev) {
+			std::vector<Vertex *> q;
+
+			dist.clear();
+			prev.clear();
+
+			for (auto v = this->adjVertices.begin(); v != this->adjVertices.end(); v++) {
+				dist[v->first] = INT_MAX;
+				prev[v->first] = NULL;
+
+				q.push_back(v->first);
+			}
+
+			dist[root] = 0;
+
+			while (! q.empty()) {
+				Vertex *u;
+
+				{
+					std::vector<Vertex *>::iterator uIt = q.begin();
+
+					for (auto it = q.begin(); it != q.end(); it++) {
+						if (dist[*it] < dist[*uIt]) {
+							uIt = it;
+						}
+					}
+
+					u = *uIt;
+
+					q.erase(uIt);
+				}
+
+				for (auto v = adjVertices[u].begin(); v != adjVertices[u].end(); v++) {
+					int alt = dist[u] + v->second;
+
+					if (alt < dist[v->first]) {
+						dist[v->first] = alt;
+						prev[v->first] = u;
+					}
+				}
+			}
+		}
+
+
+	private:
+		std::map<Vertex *, std::vector<std::pair<Vertex *, float>>> adjVertices;
+};
+
+#endif
