243f4f3f87eff1d2831808d1b51e1d2aa3788a1f
Jarosław Bielski <bielski.j@gmail.com>
2020-01-06 08:49:07 +0100

    Code refactoring

--- 2019/day24/main.cpp
+++ 2019/day24/main.cpp
@@ -12,113 +12,143 @@
 #include "common/debug.h"
 
 
-static bool isBug(char *map, int x, int y, int w, int h) {
-	if (x >= 0 && x < w && y >= 0 && y < h) {
-		return map[y * w + x] == '#';
-	}
+#define GRID_SIZE 403
 
-	return false;
-}
+struct Tile {
+	static const int W = 5;
+	static const int H = 5;
 
+	char area[W * H];
 
-static void printMap(char *map, int w, int h) {
-	printf("\n");
-	for (int y = 0; y < h; y++) {
-		for (int x = 0; x < w; x++) {
-			printf("%c", map[y * w + x]);
+	Tile(const std::vector<std::string> &data) {
+		for (int y = 0; y < data.size(); y++) {
+			for (int x = 0; x < data[y].length(); x++) {
+				if (data[y][x] == '#') {
+					this->set(x, y);
+				} else {
+					this->clear(x, y);
+				}
+			}
 		}
-		printf("\n");
 	}
-}
+
+	Tile() {
+		memset(area, '.', W * H);
+	}
+
+	Tile(const Tile &other) {
+		memcpy(this->area, other.area, W * H);
+	}
+
+	void set(int x, int y) {
+		this->area[y * W + x] = '#';
+	}
+
+	void clear(int x, int y) {
+		this->area[y * W + x] = '.';
+	}
+
+	bool isBug(int x, int y) {
+		if (x >= 0 && x < W && y >= 0 && y < H) {
+			return this->area[y * W + x] == '#';
+
+		} else {
+			return false;
+		}
+	}
+
+	void print() {
+		printf("\n");
+		for (int y = 0; y < H; y++) {
+			for (int x = 0; x < W; x++) {
+				printf("%c", area[y * W + x]);
+			}
+			printf("\n");
+		}
+	}
+
+	std::string toString() {
+		std::string ret;
+
+		for (int y = 0; y < H; y++) {
+			for (int x = 0; x < W; x++) {
+				ret.push_back(this->area[y * W + x]);
+			}
+		}
+
+		return ret;
+	}
+
+	Tile &operator=(const Tile &other) {
+		memcpy(this->area, other.area, W * H);
+
+		return *this;
+	}
+};
 
 
 int main(int argc, char *argv[]) {
-	std::vector<std::string> data = File::readAllLines(argv[1]);
+	Tile map(File::readAllLines(argv[1]));
 
-	const int w = 5;
-	const int h = 5;
+	{
+		std::map<std::string, int> combinations;
 
-	char map[w * h];
-	char newMap[w * h];
+		Tile current = map;
+		Tile tmp;
 
-	for (int y = 0; y < h; y++) {
-		for (int x = 0; x < w; x++) {
-			map[y * w + x] = data[y][x];
+		bool cont = true;
+
+		while (cont) {
+//			current.print();
+
+			combinations[current.toString()]++;
+
+			for (auto &_comb : combinations) {
+				if (_comb.second > 1) {
+					long long int sum = 0;
+
+					for (int i = 0; i < _comb.first.length(); i++) {
+						if (_comb.first.at(i) == '#') {
+							sum += (1 << (i));
+						}
+					}
+
+					PRINTF(("PART_A: %lld", sum));
+
+					cont = false;
+					break;
+				}
+			}
+
+			tmp = current;
+
+			for (int y = 0; y < Tile::H; y++) {
+				for (int x = 0; x < Tile::W; x++) {
+					int bugsCount = 0;
+
+					if (current.isBug(x - 1, y    )) bugsCount++;
+					if (current.isBug(x + 1, y    )) bugsCount++;
+					if (current.isBug(x    , y - 1)) bugsCount++;
+					if (current.isBug(x    , y + 1)) bugsCount++;
+
+					if (current.isBug(x, y)) {
+						if (bugsCount != 1) {
+							tmp.clear(x, y);
+						}
+
+					} else {
+						if (bugsCount == 1 || bugsCount == 2) {
+							tmp.set(x, y);
+						}
+					}
+				}
+			}
+
+			current = tmp;
 		}
 	}
 
-	std::map<std::string, int> combinations;
+	{
 
-	std::string c;
-//	c.reserve(w * h);
-
-	bool cont = true;
-
-	while (cont) {
-//		printMap(map, w, h);
-
-		c.clear();
-
-		for (int y = 0; y < h; y++) {
-			for (int x = 0; x < w; x++) {
-				c.push_back(map[y * w + x]);
-			}
-		}
-
-		combinations[c]++;
-
-		for (auto &_comb : combinations) {
-			if (_comb.second > 1) {
-				long long int sum = 0;
-
-				for (int i = 0; i < _comb.first.length(); i++) {
-					if (_comb.first.at(i) == '#') {
-						sum += (1 << (i));
-					}
-				}
-
-				PRINTF(("PART_A: %lld", sum));
-
-				cont = false;
-				break;
-			}
-		}
-
-		for (int y = 0; y < h; y++) {
-			for (int x = 0; x < w; x++) {
-				int bugsCount = 0;
-
-				if (isBug(map, x - 1, y, w, h)) {
-					bugsCount++;
-				}
-
-				if (isBug(map, x + 1, y, w, h)) {
-					bugsCount++;
-				}
-
-				if (isBug(map, x, y - 1, w, h)) {
-					bugsCount++;
-				}
-
-				if (isBug(map, x, y + 1, w, h)) {
-					bugsCount++;
-				}
-
-				newMap[y * w + x] = map[y * w + x];
-
-				if (isBug(map, x, y, w, h)) {
-					if (bugsCount != 1) {
-						newMap[y * w + x] = '.';
-					}
-
-				} else {
-					if (bugsCount == 1 || bugsCount == 2) {
-						newMap[y * w + x] = '#';
-					}
-				}
-			}
-		}
-
-		memcpy(map, newMap, sizeof(map));
 	}
 }
