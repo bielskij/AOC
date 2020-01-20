2d2a249ba3a53a292e81d97e10f5cc46795fdac1
Jaroslaw Bielski <bielski.j@gmail.com>
2020-01-20 12:00:29 +0100

    Solution to day2 part B

--- 2016/day2/main.cpp
+++ 2016/day2/main.cpp
@@ -6,44 +6,94 @@
 #include "common/debug.h"
 
 
+static char valuesA[] = {
+	'1', '2', '3',
+	'4', '5', '6',
+	'7', '8', '9'
+};
+
+
+static char valuesB[] = {
+	 0,    0, '1',   0,  0,
+	 0,  '2', '3', '4',  0,
+	'5', '6', '7', '8', '9',
+	 0,  'A', 'B', 'C',  0,
+	 0,    0, 'D',   0,  0,
+};
+
+
+static char getValueA(const Point<int> &pos) {
+	if (pos.x() >= 3 || pos.y() >= 3) {
+		return 0;
+	}
+
+	return valuesA[pos.y() * 3 + pos.x()];
+}
+
+
+static char getValueB(const Point<int> &pos) {
+	if (pos.x() >= 5 || pos.y() >= 5) {
+		return 0;
+	}
+
+	return valuesB[pos.y() * 5 + pos.x()];
+}
+
+
 int main(int argc, char *argv[]) {
 	auto data = File::readAllLines(argv[1]);
 
-	Point<int> pos(1, 1);
+	{
+		Point<int> pos(1, 1);
 
-	std::string code;
+		std::string code;
 
-	for (auto &l : data) {
-		for (char c : l) {
-			switch (c) {
-				case 'U':
-					if (pos.y() > 0) {
-						pos.decY();
-					}
-					break;
+		for (auto &l : data) {
+			for (char c : l) {
+				Point<int> tmpPos = pos;
 
-				case 'D':
-					if (pos.y() + 1 < 3) {
-						pos.incY();
-					}
-					break;
+				switch (c) {
+					case 'U': tmpPos.decY(); break;
+					case 'D': tmpPos.incY(); break;
+					case 'R': tmpPos.incX(); break;
+					case 'L': tmpPos.decX(); break;
+				}
 
-				case 'R':
-					if (pos.x() + 1 < 3) {
-						pos.incX();
-					}
-					break;
-
-				case 'L':
-					if (pos.x() > 0) {
-						pos.decX();
-					}
-					break;
+				if (getValueA(tmpPos) != 0) {
+					pos = tmpPos;
+				}
 			}
+
+			code.push_back(getValueA(pos));
 		}
 
-		code.push_back('0' + pos.y() * 3 + pos.x() + 1);
+		PRINTF(("PART_A: %s", code.c_str()));
 	}
 
-	PRINTF(("PART_A: %s", code.c_str()));
+	{
+		Point<int> pos(0, 2);
+
+		std::string code;
+
+		for (auto &l : data) {
+			for (char c : l) {
+				Point<int> tmpPos = pos;
+
+				switch (c) {
+					case 'U': tmpPos.decY(); break;
+					case 'D': tmpPos.incY(); break;
+					case 'R': tmpPos.incX(); break;
+					case 'L': tmpPos.decX(); break;
+				}
+
+				if (getValueB(tmpPos) != 0) {
+					pos = tmpPos;
+				}
+			}
+
+			code.push_back(getValueB(pos));
+		}
+
+		PRINTF(("PART_B: %s", code.c_str()));
+	}
 }
