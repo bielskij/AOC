5ead4bb5886911a8a99a73dd20b4eb2721e3aa49
Jaroslaw Bielski <bielski.j@gmail.com>
2020-02-04 11:27:36 +0100

    Solution to day 7 part B

--- 2016/day7/main.cpp
+++ 2016/day7/main.cpp
@@ -25,58 +25,110 @@ int main(int argc, char *argv[]) {
 	auto data = File::readAllLines(argv[1]);
 
 	int partA = 0;
+	int partB = 0;
 
 	for (auto &l : data) {
-		std::string word;
-		bool ishypernet = false;
+		std::vector<std::string> hypernets;
+		std::vector<std::string> supernets;
 
-		bool noAbbaBeforeSquare = true;
-		bool abbaInHypernet     = false;
+		std::string word;
+
+		bool inHyperNet = false;
 
 		for (int i = 0; i < l.length(); i++) {
 			char c = l[i];
 
 			if (c == '[') {
-				PRINTF(("%s - %d", word.c_str(), isAbba(word)));
-				if (! word.empty() && isAbba(word)) {
-					noAbbaBeforeSquare = false;
-				}
-
-				ishypernet = true;
+				supernets.push_back(word);
 				word.clear();
+
+				inHyperNet = true;
 
 			} else if (c == ']') {
-				PRINTF(("%s - %d", word.c_str(), isAbba(word)));
-				if (! word.empty() && isAbba(word)) {
-					abbaInHypernet = true;
-				}
-
-				ishypernet = false;
+				hypernets.push_back(word);
 				word.clear();
+
+				inHyperNet = false;
 
 			} else {
 				word += c;
 			}
+		}
 
-			if (i == l.length() - 1) {
-				if (! word.empty()) {
-					PRINTF(("%s - %d", word.c_str(), isAbba(word)));
+		if (! word.empty()) {
+			if (inHyperNet) {
+				hypernets.push_back(word);
 
-					if (ishypernet && isAbba(word)) {
-						abbaInHypernet = true;
-					}
-
-					if (! ishypernet && isAbba(word)) {
-						noAbbaBeforeSquare = false;
-					}
-				}
+			} else {
+				supernets.push_back(word);
 			}
 		}
 
-		if (! abbaInHypernet && ! noAbbaBeforeSquare) {
-			partA++;
+		// PartA
+		{
+			bool abbaInSupernet = false;
+			bool abbaInHypernet = false;
+
+			for (auto &s : supernets) {
+				if (isAbba(s)) {
+					abbaInSupernet = true;
+					break;
+				}
+			}
+
+			for (auto &s : hypernets) {
+				if (isAbba(s)) {
+					abbaInHypernet = true;
+					break;
+				}
+			}
+
+			if (! abbaInHypernet && abbaInSupernet) {
+				partA++;
+			}
+		}
+
+		// PartB
+		{
+			std::vector<std::string> aba;
+
+			for (auto &s : supernets) {
+				for (int i = 0; i < s.length() - 2; i++) {
+					if (s[i] == s[i + 2] && s[i] != s[i + 1]) {
+						aba.push_back(s.substr(i, 3));
+					}
+				}
+			}
+
+			if (! aba.empty()) {
+				bool hasAba = false;
+
+				for (auto &a : aba) {
+					std::string bab;
+
+					bab.push_back(a[1]);
+					bab.push_back(a[0]);
+					bab.push_back(a[1]);
+
+					for (auto &h : hypernets) {
+						if (h.find(bab) != std::string::npos) {
+							hasAba = true;
+							break;
+						}
+					}
+
+					if (hasAba) {
+						break;
+					}
+				}
+
+				if (hasAba) {
+					partB++;
+				}
+			}
 		}
 	}
 
 	PRINTF(("PART_A: %d", partA));
+	PRINTF(("PART_B: %d", partB));
 }
