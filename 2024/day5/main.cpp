f1cf696d9c80f5de55adf28add9338a81c58450a
Jarosław Bielski <bielski.j@gmail.com>
2024-12-05 23:06:28 +0100

    Solution to part B day 5

--- 2024/day5/main.cpp
+++ 2024/day5/main.cpp
@@ -6,6 +6,23 @@
 #define DEBUG_LEVEL 5
 #include "common/debug.h"
 
+
+static int checkOrder(const std::map<int, std::vector<int>> &rules, const std::vector<int> &pages) {
+	for (int i = 0; i < pages.size() - 1; i++) {
+		auto ruleIt = rules.find(pages[i]);
+		if (ruleIt == rules.end()) {
+			return i;
+		}
+
+		if (std::find(ruleIt->second.begin(), ruleIt->second.end(), pages[i + 1]) == ruleIt->second.end()) {
+			return i + 1;
+		}
+	}
+
+	return -1;
+}
+
+
 int main(int argc, char *argv[]) {
 	auto lines = File::readAllLines(argv[1]);
 
@@ -14,6 +31,7 @@ int main(int argc, char *argv[]) {
 	bool parseRules = true;
 
 	int partA = 0;
+	int partB = 0;
 
 	for (const auto &l : lines) {
 		if (l.empty()) {
@@ -30,25 +48,29 @@ int main(int argc, char *argv[]) {
 		} else {
 			auto toks = utils::toIntV<int>(utils::strTok(l, ','));
 
-			bool valid = true;
-
-			for (int i = 0; i < toks.size() - 1; i++) {
-				const auto &rule = rules[toks[i]];
-
-				auto it = std::find(rule.begin(), rule.end(), toks[i + 1]);
-				if (it == rule.end()) {
-					valid = false;
-					break;
-				}
-			}
-
-			if (valid) {
+			auto checkRet = checkOrder(rules, toks);
+			if (checkRet < 0) {
 				partA += toks[toks.size() / 2];
+
+			} else {
+				std::sort(toks.begin(), toks.end(), [&rules](int l, int r) {
+					auto ruleIt = rules.find(r);
+					if (ruleIt != rules.end()) {
+						if (std::find(ruleIt->second.begin(), ruleIt->second.end(), l) != ruleIt->second.end()) {
+							return true;
+						}
+					}
+
+					return false;
+				});
+
+				partB += toks[toks.size() / 2];
 			}
 		}
 	}
 
 	PRINTF(("PART_A: %d", partA));
+	PRINTF(("PART_B: %d", partB));
 
 	return 0;
 }
